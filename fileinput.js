1ad9b95a0d38c557cf50598ac0a13dd7828d9afc
yoavgurevich <ygurevich@ymail.com>
2014-05-09 12:56:47 -0400

    Regex tweaks to accept a broader variety of URLs

--- fileinput.js
+++ fileinput.js
@@ -1,13 +1,15 @@
 var fs = require('fs');
 var parse = require('./linkparser.js');
 
-var data = process.argv[2];
+if(process.argv){
+    var data = process.argv[2];
 
-fs.readFile(data, 'utf8', function(err, data){
-    if(err)
-        throw err;
-    if(!data)
-        throw "no file argument given";
-    if(data.match(/^.+[\.md|\.markdown]$/))
-        parse(data);
-});
+    fs.readFile(data, 'utf8', function(err, data){
+        if(err)
+            throw err;
+        if(!data)
+            throw "no file argument given";
+        if(data.match(/^.+[\.md|\.markdown]$/))
+            parse(data);
+    });
+}
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 module.exports = function( mdString ){
-    return mdString.match(/\[([^\]]+)\]\(([^)]+)\)/g).toString().match(/http:\/\/([a-zA-Z0-9_\-]+)([\.][a-zA-Z0-9_\-]+)+([/][a-zA-Z0-9\~_\-]*)+([\.][a-zA-Z0-9_\-]+)*/g);
+    return mdString.match(/\[([^\]]+)\]\(([^)]+)\)/g).toString().match(/[a-zA-Z0-9]+(:\/\/|.)([a-zA-Z0-9_\-]+)([\.][a-zA-Z0-9_\-]+)+([/][a-zA-Z0-9\~_\-]*)+([\.][a-zA-Z0-9_\-]+)*/g);
 };
