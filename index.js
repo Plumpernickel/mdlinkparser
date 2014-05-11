704183ec12d24b868a206e9ebc88277796f3defb
yoavgurevich <ygurevich@ymail.com>
2014-05-11 13:20:42 -0400

    Added compatibility for Require.js on top of Node.js functionality

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-module.exports = function( mdString ){
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
+
+define(function(require) {
+    //The value returned from the function is
+    //used as the module export visible to Node.
+    return function ( mdString ){
     return mdString.match(/\[([^\]]+)\]\(([^)]+)\)/g).toString().match(/[a-zA-Z0-9]+(:\/\/|.)([a-zA-Z0-9_\-]+)([\.][a-zA-Z0-9_\-]+)+([/][a-zA-Z0-9\~_\-]*)+([\.][a-zA-Z0-9_\-]+)*/g);
-};
+    };
+});
